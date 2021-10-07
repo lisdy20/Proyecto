a5bb6db46d8b17cf72543451b8732383410ede48
Axel Castillo <axel-cas@hotmail.com>
2021-10-07 01:36:46 -0600

    added function to calculated available rooms, added print route pdf

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'voluntario',
     'asistencia',
     'authuser',
+    'easy_pdf'
     
 
 ]
@@ -137,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls import url
+from ruta.views import RutaPDF
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    url(r"^ruta-de-riesgo/(?P<id>)" , RutaPDF.as_view())
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
